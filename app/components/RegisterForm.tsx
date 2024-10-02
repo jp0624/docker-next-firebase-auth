1411b4cb1e1f9206b45756152cd1c4849cc84755
jp0624 <jp0624@gmail.com>
2024-10-02 15:44:49 -0400

    added more special chars to regex

--- app/components/RegisterForm.tsx
+++ app/components/RegisterForm.tsx
@@ -23,7 +23,7 @@ import { useRouter } from 'next/navigation'
 import { INTERIOR_ROUTE } from '@/constants/routes'
 
 const passwordValidation = new RegExp(
-	/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*,.<>|-=_+?])[A-Za-z\d!@#$%^&*,.<>|-=_+?]{8,}$/
+	/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&,./<>?;':=_+])[A-Za-z\d@$!%*?&,./<>?;':=_+]{8,}$/
 )
 const emailValidation = new RegExp(
 	/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/
